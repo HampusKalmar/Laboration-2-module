7247b5c802df3eabc8eebd84ca2c0cd0aabf4813
Hampus Tuisku <ht222mi@student.lnu.se>
2023-09-12 13:15:44 +0200

    Initial commit

---
+++ src/main/java/ProjectGeneratorImpl.java
@@ -0,0 +1,9 @@
+import java.io.File;
+import java.io.IOException;
+
+public class ProjectGeneratorImpl implements ProjectGenerator {
+    @Override
+    public void createFolderStructure(String path, String projectName) {
+        
+    }
+}
